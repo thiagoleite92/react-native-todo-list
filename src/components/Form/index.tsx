1cc2d74b300c3a7de4d19cd85ae1944e519ce7d1
Thiago Leite <leite.tjs@gmail.com>
2024-11-17 19:03:37 -0300

    feat: empty list styles

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,21 +1,36 @@
 import { useState } from 'react';
-import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Pressable, TextInput, View } from 'react-native';
 import { colors } from '../../theme/colors';
 import { PlusSign } from '../PlusSign';
 import { styles } from './styles';
 
 export function Form() {
   const [text, setText] = useState<string>('');
+  const [inputIsFocused, setInputIsFocused] = useState<boolean>(false);
+
+  const handleInputStyles = (isFocused: boolean) => {
+    setInputIsFocused(isFocused);
+  };
 
   return (
     <View style={styles.form}>
-      <TextInput
-        value={text}
-        onChangeText={setText}
-        style={styles.input}
-        placeholder="Adicione uma nova tarefa"
-        placeholderTextColor={colors.gray300}
-      />
+      <View
+        style={[
+          styles.inputContainer,
+          inputIsFocused && styles.inputContainerOnFocus,
+        ]}
+      >
+        <TextInput
+          value={text}
+          onChangeText={setText}
+          style={[styles.input, inputIsFocused && styles.inputFocused]}
+          placeholder="Adicione uma nova tarefa"
+          placeholderTextColor={colors.gray300}
+          onFocus={() => handleInputStyles(true)}
+          onBlur={() => handleInputStyles(false)}
+          maxLength={50}
+        />
+      </View>
       <Pressable style={styles.button}>
         <PlusSign />
       </Pressable>
--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -3,27 +3,43 @@ import { colors } from '../../theme/colors';
 
 export const styles = StyleSheet.create({
   form: {
-    width: 327,
     justifyContent: 'center',
     alignItems: 'center',
     marginRight: 'auto',
     marginLeft: 'auto',
     marginTop: -30,
     flexDirection: 'row',
-    gap: 4,
+    gap: 6,
+  },
+  inputContainer: {
+    width: 271,
+    borderRadius: 8,
+    marginHorizontal: 'auto',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  inputContainerOnFocus: {
+    borderColor: colors.purple,
+    borderWidth: 1,
   },
   input: {
-    width: 327,
+    width: '100%',
     height: 54,
-    borderWidth: 1,
     padding: 16,
     backgroundColor: colors.gray500,
     borderRadius: 8,
+    color: colors.gray200,
+    fontSize: 16,
+    borderWidth: 1,
+  },
+  inputFocused: {
+    borderColor: 'red',
+    borderStyle: 'dotted',
   },
   button: {
     width: 52,
     height: 54,
-    backgroundColor: colors.blue,
+    backgroundColor: colors.blueDark,
     borderRadius: 8,
     justifyContent: 'center',
     alignItems: 'center',
